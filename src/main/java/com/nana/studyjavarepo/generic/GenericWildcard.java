066c2187708b473e702db43ec4effe70d64e5814
xingnn <xingnn@tuya.com>
2020-04-23 14:27:42 +0800

    modify| 泛型上下限

--- src/main/java/com/nana/studyjavarepo/generic/GenericWildcard.java
+++ src/main/java/com/nana/studyjavarepo/generic/GenericWildcard.java
@@ -2,23 +2,37 @@ package com.nana.studyjavarepo.generic;
 
 /**
  * 泛型-类型通配符
- *
+ * <p>
  * (1)?(类型实参)： List<?> list
  * (2)上限通配符: List<? extends Number> Number的子类
  * (3)下限通配符: List <? super Integer>
+ *
  * @author xingnana
  * @create 2020-04-22
  */
-public class GenericWildcard<T> {
+public class GenericWildcard<T extends Number> {
     private T info;
 
+    public GenericWildcard(T info){
+        this.info=info;
+    }
+
     public T getInfo() {
+        System.out.println("设置info:" + info);
         return info;
     }
 
-    public void setInfo(T info) {
-        this.info = info;
-        System.out.println("设置info:" + info);
+
+    public static void main(String[] args) {
+        GenericWildcard<Integer> stringGenericWildcard=new GenericWildcard(1111);
+
+        GenericWildcard<Float> floatGenericWildcard=new GenericWildcard<>(22.22f);
+        GenericWildcard<Double> doubleGenericWildcard=new GenericWildcard<>(2.5677);
+
+        stringGenericWildcard.getInfo();
+        floatGenericWildcard.getInfo();
+        doubleGenericWildcard.getInfo();
+
     }
 
 
