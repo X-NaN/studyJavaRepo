79280b38e3ea0b85478d0b2c1eefdab73d922d7b
xingnn <xingnn@tuya.com>
2020-04-30 18:08:47 +0800

    modify| 列表分组lambada

--- src/main/java/com/nana/studyjavarepo/entity/User.java
+++ src/main/java/com/nana/studyjavarepo/entity/User.java
@@ -16,6 +16,8 @@ public class User {
 
     private String gender;
 
+    private Integer age;
+
 
 
 }
--- src/main/java/com/nana/studyjavarepo/lambada/ListLambadaDemo.java
+++ src/main/java/com/nana/studyjavarepo/lambada/ListLambadaDemo.java
@@ -4,6 +4,8 @@ import com.nana.studyjavarepo.entity.User;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * 列表 应用lambada
@@ -15,9 +17,12 @@ public class ListLambadaDemo {
     private static List<User> userList=new ArrayList<>();
 
     static {
-        userList.add(new User("1","lily","female"));
-        userList.add(new User("2","lucy","female"));
-        userList.add(new User("3","jack","male"));
+        userList.add(new User("1","lily","female",20));
+        userList.add(new User("2","lucy","female",20));
+        userList.add(new User("3","jack","male",17));
+        userList.add(User.builder().userId("4").userName("jacy").gender("male").age(17).build());
+        userList.add(User.builder().userId("5").userName("apollo").gender("male").age(16).build());
+
     }
 
     public static void main(String[] args) {
@@ -38,5 +43,9 @@ public class ListLambadaDemo {
 
         System.out.println("先过滤，再遍历");
         userList.stream().filter(item->item.getGender().equals("female")).forEach(item-> System.out.println(item));
+
+        System.out.println("列表分组");
+        Map<Integer,List<User>> collectResult=userList.stream().collect(Collectors.groupingBy(item->item.getAge()));
+        System.out.println(collectResult);
     }
 }
