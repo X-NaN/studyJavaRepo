386cecc424c79abc201e09e2e0eccdd8bea96cf2
xingnn <xingnn@tuya.com>
2020-07-29 23:26:29 +0800

    add| 插入思想的全排列

--- src/main/java/com/nana/studyjavarepo/leetcode/simple/Arrange.java
+++ src/main/java/com/nana/studyjavarepo/leetcode/simple/Arrange.java
@@ -1,5 +1,10 @@
 package com.nana.studyjavarepo.leetcode.simple;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * 排列
  *
@@ -15,6 +20,10 @@ public class Arrange {
 
     /**
      * 全排列
+     * 思想：
+     * 如ABC
+     * 第0位，跟第自己换，跟第1位换，跟第n-1位换。[A]BC ->ABC,BAC,CBA
+     * --》A[B]C 第自己换，跟C换，A[B]C->ABC,ACB
      *
      * @param arr
      * @param index
@@ -38,9 +47,61 @@ public class Arrange {
         }
     }
 
+    /**
+     * 深拷贝
+     *
+     * @param data
+     * @return
+     */
+    private static List<String> copy(List<String> data) {
+        List<String> newData = new ArrayList<>();
+        for (int i = 0; i < data.size(); i++) {
+            newData.add(data.get(i));
+        }
+
+        return newData;
+    }
+
+
+    /**
+     * 全排列
+     * 插入
+     *
+     * @param arr
+     * @return
+     */
+    public static List<List<String>> arrange1(List<String> arr) {
+        if (arr.size() == 1) {
+            List list = new ArrayList();
+            list.add(arr);
+            return list;
+        }
+        List<List<String>> result = new ArrayList<>();
+        for (int i = 0; i < arr.size(); i++) {
+            List<String> dataTmp = copy(arr);
+            dataTmp.remove(i);
+            String a = arr.get(i);
+            List tmp = arrange1(dataTmp);
+            for (int j = 0; j < tmp.size(); j++) {
+                List<String> sList = (List<String>) tmp.get(j);
+                sList.add(0, a);
+                result.add(sList);
+            }
+
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         String s = "AAB";
         arrange(s.toCharArray(), 0);
+        System.out.println("==============");
+        List<String> s1 = new ArrayList<>();
+        s1.add("A");
+        s1.add("B");
+        s1.add("C");
+        System.out.println(arrange1(s1));
     }
 
 }
